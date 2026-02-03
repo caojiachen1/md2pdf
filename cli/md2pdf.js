b250bab4e39f30871f4af81a54cdcaa193353213
caojiachen1 <caojiachen1@163.com>
2026-02-03 19:23:44 +0800

    fix typo

--- cli/md2pdf.js
+++ cli/md2pdf.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Markdown LaTeX to PDF Converter - 主入口文�?
+ * Markdown LaTeX to PDF Converter - 主入口文件
  * 
  * 功能：将包含LaTeX数学公式的Markdown文件转换为PDF
  * 使用：node md2pdf.js <markdown文件路径> [输出PDF路径]
